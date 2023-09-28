c07eabbd369e22468c84b50de6866bf0b6e2dd98
Anatoly V Novikov <46084344+nawww83@users.noreply.github.com>
2023-09-28 22:33:40 +0300

    Update WaveformAnalyzer.m

--- WaveformAnalyzer.m
+++ WaveformAnalyzer.m
@@ -36,20 +36,36 @@ classdef WaveformAnalyzer < handle
         noiseMeanPower
         modulationType
         waveformDuration
-        dopplershift
+        dopplerShift
+    end
+    
+    properties (Access = private)
+       waveformInfo
+       waveformSource
     end
 
     methods
         function this = WaveformAnalyzer()
             % Конструктор класса. Чтение waveform-ы во временной области и структуры с информацией
             % необходимой для дальнейшей обработки данных и заполнения полей класса
+            this.waveformInfo = load('waveform\waveformInfo.mat').info;
+            this.waveformSource = load('waveform\waveformSource.mat').rxWaveform;
         end
 
         function calcWaveformParameters(this)
-
+            this.waveformDuration = length(this.waveformSource) / this.waveformInfo.SampleRate; % s
+            this.waveformMeanPower = mean( this.waveformSource .* conj(this.waveformSource) );
+            I = real(this.waveformInfo.payloadSymbols);
+            Q = imag(this.waveformInfo.payloadSymbols);
+            dI = 2*min(abs(I)); % grid I step
+            dQ = 2*min(abs(Q)); % grid Q step
+            N = floor((max(I) - min(I))/dI + 0.5) + 1;
+            M = floor((max(Q) - min(Q))/dQ + 0.5) + 1;
+            this.modulationType = N*M; % only for QAM-X (rectangle grid)
+            this.channelBandwidth = this.waveformInfo.Nfft/this.waveformDuration; % N * frequency step, Hz
         end
 
-        function calcdopplerSHift
+        function calcDopplerShift
 
         end
 
