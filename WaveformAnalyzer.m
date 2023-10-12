6888402847dd8b4644977df1dc5e7cbb2a203528
Georges <gtarasian@yandex.ru>
2023-10-13 02:23:08 +0300

    Выполнено задание по п.п. 1-4

--- WaveformAnalyzer.m
+++ WaveformAnalyzer.m
@@ -29,32 +29,113 @@ classdef WaveformAnalyzer < handle
     %       waveformDuration  - длина анализируемого сигнала
     %
 
+    properties (Access = private)
+        waveformSource
+        waveformInfo
+    end
+    
     properties
         rmsEvm
         waveformMeanPower
-        channelBandwidth
+        channelBandwidth_Hz
         noiseMeanPower
         modulationType
-        waveformDuration
-        dopplershift
+        waveformDuration_mcs
+        dopplerShift_Hz
     end
 
     methods
-        function this = WaveformAnalyzer()
+        function this = WaveformAnalyzer(waveformSourceFileName, waveformInfoFileName)
             % Конструктор класса. Чтение waveform-ы во временной области и структуры с информацией
             % необходимой для дальнейшей обработки данных и заполнения полей класса
+
+            this.waveformSource = load(waveformSourceFileName).rxWaveform;
+            this.waveformInfo = load(waveformInfoFileName).info;
         end
 
         function calcWaveformParameters(this)
-
+            
+            % вычисление средней мощности сигнала
+            this.waveformMeanPower = mean(abs(this.waveformSource).^2);
+            
+            % вычисление разноса между поднесущими в Гц
+            subCarrierBandwidth_Hz = this.waveformInfo.SampleRate/this.waveformInfo.Nfft;
+            % вычисление полосы канала в Гц
+            this.channelBandwidth_Hz = subCarrierBandwidth_Hz*this.waveformInfo.subCarriersCount;
+            
+            % определяем тип модуляции
+            this.modulationType = this.getModulationType();
+                       
+            % вычисление длительности сигнала в мкс 
+            this.waveformDuration_mcs = length(this.waveformSource)/this.waveformInfo.SampleRate*1e6;
+            
+            
         end
 
-        function calcdopplerSHift
-
+        function modType = getModulationType(this) 
+            % метод расчета типа модуляции
+            % рассчитываем по известным символам waveformInfo.payloadSymbols
+            
+            % перебираем из заданных видов модуляции. 
+            % в дальнейшем можно добавить другие виды
+            checkModulations = ["QPSK", "16QAM", "64QAM", "256QAM"];
+            errs = zeros(1, length(checkModulations));
+            
+            % вычисляем ошибку для разных видов модуляции
+            % между исходными payloadSymbols и проверочными символами для разных видов модуляции 
+            payloadQAM4Check = qammod(qamdemod(this.waveformInfo.payloadSymbols,4,'UnitAveragePower',true),4,'UnitAveragePower',true);
+            errs(1) = mean(abs(payloadQAM4Check-this.waveformInfo.payloadSymbols));
+            payloadQAM16Check = qammod(qamdemod(this.waveformInfo.payloadSymbols,16,'UnitAveragePower',true),16,'UnitAveragePower',true);
+            errs(2) = mean(abs(payloadQAM16Check-this.waveformInfo.payloadSymbols));
+            payloadQAM64Check = qammod(qamdemod(this.waveformInfo.payloadSymbols,64,'UnitAveragePower',true),64,'UnitAveragePower',true);
+            errs(3) = mean(abs(payloadQAM64Check-this.waveformInfo.payloadSymbols));
+            payloadQAM256Check = qammod(qamdemod(this.waveformInfo.payloadSymbols,256,'UnitAveragePower',true),256,'UnitAveragePower',true);
+            errs(4) = mean(abs(payloadQAM256Check-this.waveformInfo.payloadSymbols));
+            
+            % определяем модуляцию по минимуму ошибки
+            [minError, minErrorIndx] = min(errs);
+            modType = checkModulations(minErrorIndx);
+        end
+        
+        function calcDopplerShift(this)
+            % метод расчета доплеровского сдига
+            % вычисляем сдвиг по набегу фазы между отсчетами префикса и последней частью OFDM символа 
+            % реализовано вычисление только по первому длинному префиксу
+            % в дальнейшем нужно реализовать вычисление для каждого OFDM символа
+            
+            cpLength = this.waveformInfo.CyclicPrefixLengths(1);
+            % отсчеты префикса
+            cp = this.waveformSource(1:cpLength);
+            % отсчеты символа
+            ofdm = this.waveformSource(cpLength+1:this.waveformInfo.SymbolLengths(1));
+            
+            % вычисляем набег фазы для каждого отсчета
+            dphi = angle(ofdm(end-cpLength+1:end).*conj(cp));
+            % усредняем по всем отсчетам
+            meanDPhi = mean(dphi(1:end-this.waveformInfo.Windowing));
+            % вычисляем длительность OFDM символа в сек.
+            durOFDM_s = this.waveformInfo.Nfft/this.waveformInfo.SampleRate;   
+            
+            % вычисление частоты доплеровского сдвига
+            this.dopplerShift_Hz = (meanDPhi/durOFDM_s)/(2*pi);
         end
 
         function plotPowerSpectrumDensity(this)
-
+            % метод вывода графика спектральной плотности мощности
+            % плотность мощности отнормирована таким образом,
+            % чтобы суммарная мощность в частотной области совпадала с
+            % суммарной мощностью во временной области
+            
+            fftSmplCount = length(this.waveformSource);
+            waveformFFT = fftshift(fft(this.waveformSource)./sqrt(fftSmplCount));
+            waveformPowerDensity_dB = 20*log10(abs(waveformFFT));
+            freq = (0:fftSmplCount-1)-fftSmplCount/2;
+            freq = freq.*(this.waveformInfo.SampleRate/1000000/fftSmplCount);
+            plot(freq, waveformPowerDensity_dB);
+            title('Power Spectrum Density');
+            xlabel('Freq, MHz');
+            ylabel('Power Density, dB');
+            
         end
 
         function plotPayloadConstellation(this)
--- waveformAnalyzerSimulationScenario.m
+++ waveformAnalyzerSimulationScenario.m
@@ -1,1 +1,24 @@
 % Скрипт для запуска waveformAnalyxer
+clc
+clear
+addpath waveform/
+
+% Запуск конструктора класса WaveformAnalyzer
+waveformAnalyzerObject = WaveformAnalyzer('waveformSource.mat', 'waveformInfo.mat');
+
+% вычисляем параметры сигнала
+waveformAnalyzerObject.calcWaveformParameters();
+sprintf('waveformMeanPower: %f',waveformAnalyzerObject.waveformMeanPower)
+sprintf('channelBandwidth_Hz: %9.0f',waveformAnalyzerObject.channelBandwidth_Hz)
+sprintf('modulationType: %s', waveformAnalyzerObject.modulationType)
+sprintf('waveformDuration_mcs: %f', waveformAnalyzerObject.waveformDuration_mcs)
+
+% вычисляем доплеровский сдвиг 
+waveformAnalyzerObject.calcDopplerShift();
+sprintf('dopplerShift_Hz: %5.1f',waveformAnalyzerObject.dopplerShift_Hz)
+
+% вывод графика спектральной плотности мощности
+waveformAnalyzerObject.plotPowerSpectrumDensity();
+
+
+
