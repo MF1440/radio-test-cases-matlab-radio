89ab53bbc3420e306ed9fe1027b457527408240e
Stanislav Krikunov <skrikunov@MacBook-Pro-Stanislav.local>
2024-04-09 21:02:14 +0300

    Task completed

--- WaveformAnalyzer.m
+++ WaveformAnalyzer.m
@@ -36,29 +36,122 @@ classdef WaveformAnalyzer < handle
         noiseMeanPower
         modulationType
         waveformDuration
-        dopplershift
+        dopplerShift
+        
+        % additional properties for conveinience
+        dopplerShiftAllSym
+        waveformInfo
+        waveformSource
+        uniqueSymbols
     end
 
     methods
-        function this = WaveformAnalyzer()
-            % Конструктор класса. Чтение waveform-ы во временной области и структуры с информацией
-            % необходимой для дальнейшей обработки данных и заполнения полей класса
+        function this = WaveformAnalyzer(waveformInfoFile, waveformSourceFile)
+            % read information about data
+            this.waveformInfo = load('waveform/waveformInfo.mat').info;
+            % read data waveform
+            this.waveformSource = load('waveform/waveformSource.mat').rxWaveform;
         end
 
         function calcWaveformParameters(this)
+            % mean power
+            this.waveformMeanPower = mean(abs(this.waveformSource).^2);
 
+            % find channel bandwidth [Hz]
+            scs = this.waveformInfo.SampleRate / this.waveformInfo.Nfft; % subcarrier spacing [Hz]
+            this.channelBandwidth = scs * this.waveformInfo.subCarriersCount;
+
+            % detect modulation type
+            % assume that all symbols are presented in payloadSymbols)
+            this.uniqueSymbols = unique(this.waveformInfo.payloadSymbols);
+            numOfUniqueSymbols = length(this.uniqueSymbols);
+
+            switch numOfUniqueSymbols
+               case 4
+                  this.modulationType = 'QPSK';
+               case 16
+                  this.modulationType = 'QAM16';
+                case 64
+                  this.modulationType = 'QAM64';
+                case 256
+                  this.modulationType = 'QAM256';
+                case 1024
+                  this.modulationType = 'QAM1024';
+               otherwise
+                  this.modulationType = 'Unknown modulation type';
+                  print('Unknown modulation type');
+            end
+
+            % calculate doppler shift
+            this.calcDopplerSHift();
+
+            % calculate waveform duration [s]
+            this.waveformDuration = length(this.waveformSource) / this.waveformInfo.SampleRate;
+        end
+        
+        function calcDopplerSHift(this)
+            % cyclic prefix correlation-based Doppler Frequency shift estimation
+
+            Nfft = this.waveformInfo.Nfft;
+            Fs = this.waveformInfo.SampleRate;
+            slotCorr = 0;
+            for symIdx = 1:length(this.waveformInfo.CyclicPrefixLengths)
+                lenCP = this.waveformInfo.CyclicPrefixLengths(symIdx);
+
+                % cyclic prefix for the current OFDM symbol
+                prefixStart = 1 + Nfft * (symIdx - 1);
+                prefixEnd = prefixStart + lenCP - 1;
+                symCPrefix = this.waveformSource(prefixStart:prefixEnd);
+
+                % tail for the current OFDM symbol
+                tailStart = 1 + Nfft * symIdx - lenCP;
+                tailEnd = Nfft * symIdx;
+                symTail = this.waveformSource(tailStart:tailEnd);
+
+                % correlation within OFDM symbol (tail and CP)
+                symCorr = sum(symTail .* conj(symCPrefix));
+                slotCorr = slotCorr + symCorr;
+
+                % doppler shift for the current symbol
+                this.dopplerShiftAllSym{symIdx} = - Fs * (angle(symCorr) / (2 * pi * Nfft));
+            end
+            % doppler shift for the whole slot
+            this.dopplerShift = -Fs * (angle(slotCorr) / (2 * pi * Nfft));
         end
 
-        function calcdopplerSHift
+        function plotPowerSpectrumDensity(this, fc)
+            % read parameters
+            Nfft = this.waveformInfo.Nfft;
+            Fs = this.waveformInfo.SampleRate;
 
-        end
+            % estimate PSD [linear scale]
+            [psd,f] = pwelch(this.waveformSource, Nfft, Nfft/2, Nfft, Fs);
 
-        function plotPowerSpectrumDensity(this)
+            % go to [dB] scale
+            psd_dB = pow2db(psd);
 
+            % visualize PSD
+            figure(1);
+            plot((f - Fs/2) / 1e6, fftshift(psd_dB));
+            grid;
+            xlabel('Frequency [MHz]');
+            ylabel('Power [dB]');
+            ylim([min(psd_dB), max(psd_dB) / 1.25]);
+            xlim([-Fs/2e6, Fs/2e6]);
+            title('Power Spectral Density (PSD)');
         end
 
         function plotPayloadConstellation(this)
-
+            figure(2);
+            scatter(real(this.uniqueSymbols),imag(this.uniqueSymbols));
+            max_val = 1.5*round(max(abs(this.uniqueSymbols)))/2;
+            grid;
+            xlim([-max_val,max_val]);
+            ylim([-max_val,max_val]);
+            xticks([-max_val:0.25:max_val]);
+            yticks([-max_val:0.25:max_val]);
+            axis('equal');
+            title([this.modulationType,' constellation']);
         end
 
         function calcEvmPerformance(this)
--- waveformAnalyzerSimulationScenario.m
+++ waveformAnalyzerSimulationScenario.m
@@ -1,1 +1,27 @@
-% Скрипт для запуска waveformAnalyxer
+close all;
+clear;
+clc;
+
+% initial data paths
+waveformInfoFile = 'waveform/waveformInfo.mat';
+waveformSourceFile = 'waveform/waveformSource.mat';
+
+% create waveform analyzer
+wfa = WaveformAnalyzer(waveformInfoFile, waveformSourceFile);
+
+% calcualte waveform parameters
+wfa.calcWaveformParameters();
+% plot power spectral density
+wfa.plotPowerSpectrumDensity();
+% constellation visualization
+wfa.plotPayloadConstellation();
+
+% show all calculated parameters
+fprintf('Waveform Mean Power: %f \n', wfa.waveformMeanPower);
+fprintf('Channel bandwidth [MHz]: %.3f \n', wfa.channelBandwidth / 1e6);
+fprintf('Modulation type: %s \n', wfa.modulationType);
+fprintf('Waveform duration [ms]: %3f \n', wfa.waveformDuration * 1e3);
+fprintf('Doppler shift (slot) [Hz]: %.3f \n', wfa.dopplerShift);
+for sym = 1:14
+    fprintf(['Doppler shift (symbol ',num2str(sym),') [Hz]: %.3f \n'], wfa.dopplerShiftAllSym{sym});
+end
