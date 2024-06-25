5dabdc160c1a314e8ba37b8a12e98a682195117c
MPShvetsov <misha_9930@mail.ru>
2024-06-25 15:37:39 +0300

    Выполнил тестовое задание

--- waveformAnalyzerSimulationScenario.m
+++ waveformAnalyzerSimulationScenario.m
@@ -1,1 +1,19 @@
-% РЎРєСЂРёРїС‚ РґР»СЏ Р·Р°РїСѓСЃРєР° waveformAnalyxer
+clc
+clear
+addpath waveform/
+
+% Чтение baseband сигнала во временной области и структуры с его
+% параметрами
+waveformSource = load('waveformSource.mat');
+waveformInfo = load('waveformInfo.mat');
+
+% Запуск конструктора класса
+waveformAnalyserObject = WaveformAnalyzer(waveformSource, waveformInfo);
+% расчет и вывод в консоль параметров радиоволны
+waveformAnalyserObject.calcWaveformParameters();
+waveformAnalyserObject.printParametrs();
+% построение спектральной плотности сигнала
+waveformAnalyserObject.plotPowerSpectrumDensity();
+% построение созвездия переданных символов
+waveformAnalyserObject.plotPayloadConstellation();
+disp("-----End Programm-----");
