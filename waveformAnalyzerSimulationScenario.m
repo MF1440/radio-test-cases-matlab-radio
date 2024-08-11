f178105d586541b59525280a24808a6c8bbff41a
DmitriiADolgikh <dolgikhda@gmail.com>
2024-08-11 20:57:32 +0300

    corrections according to README.md

--- waveformAnalyzerSimulationScenario.m
+++ waveformAnalyzerSimulationScenario.m
@@ -1,1 +1,27 @@
 % Скрипт для запуска waveformAnalyxer
+
+%%
+clc
+clear
+addpath waveform/
+%%
+filePathInfo = "waveform/waveformInfo.mat";
+filePathSource = "waveform/waveformSource.mat";
+
+load(filePathInfo) 
+load(filePathSource)
+
+% Запуск конструктора класса
+waveformAnalyzerObject = WaveformAnalyzer(info, rxWaveform);
+
+% Определяем центральную частоту спектра (Гц)
+centralFrequency = 11.7e9;
+
+% Определяем скорость приемника отностительно передатчика (м/с)
+radialSpeed = 100 * 1e3 / 3600;
+
+% Расчет Доплеровского сдвига 
+waveformAnalyzerObject.calcDopplerShift(centralFrequency, radialSpeed);
+
+% построение psd
+waveformAnalyzerObject.plotPowerSpectrumDensity(rxWaveform, info.SampleRate);
